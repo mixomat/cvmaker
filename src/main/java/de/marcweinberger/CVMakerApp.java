92bcef8d54b2fdaa848f97d2a3aaf5e6b24009ac
Marc Weinberger <marc.weinberger@me.com>
2015-07-09 22:57:36 +0200

    initial commit

---
+++ src/main/java/de/marcweinberger/CVMakerApp.java
@@ -0,0 +1,12 @@
+package de.marcweinberger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CVMakerApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CVMakerApp.class, args);
+    }
+}
