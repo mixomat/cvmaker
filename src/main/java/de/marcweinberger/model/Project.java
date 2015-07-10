3c8c54139876d1b6a3cc5413384fa3c3d52d5ae4
Marc Weinberger <marc.weinberger@me.com>
2015-07-10 13:45:16 +0200

    created project entity

---
+++ src/main/java/de/marcweinberger/model/Project.java
@@ -0,0 +1,104 @@
+package de.marcweinberger.model;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+/**
+ * Project entity.
+ *
+ * @author Marc Weinberger, marc.weinberger@me.com
+ * @since 10.07.15
+ */
+public class Project {
+
+	private String id;
+	private String title;
+	private String role;
+	private String customer;
+	private String sector;
+	private String description;
+	private LocalDate startDate;
+	private LocalDate endDate;
+	private LocalDateTime createdAt;
+	private LocalDateTime updatedAt;
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public void setCreatedAt(LocalDateTime createdAt) {
+		this.createdAt = createdAt;
+	}
+
+	public String getCustomer() {
+		return customer;
+	}
+
+	public void setCustomer(String customer) {
+		this.customer = customer;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public LocalDate getEndDate() {
+		return endDate;
+	}
+
+	public void setEndDate(LocalDate endDate) {
+		this.endDate = endDate;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getRole() {
+		return role;
+	}
+
+	public void setRole(String role) {
+		this.role = role;
+	}
+
+	public String getSector() {
+		return sector;
+	}
+
+	public void setSector(String sector) {
+		this.sector = sector;
+	}
+
+	public LocalDate getStartDate() {
+		return startDate;
+	}
+
+	public void setStartDate(LocalDate startDate) {
+		this.startDate = startDate;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
+
+	public void setUpdatedAt(LocalDateTime updatedAt) {
+		this.updatedAt = updatedAt;
+	}
+}
