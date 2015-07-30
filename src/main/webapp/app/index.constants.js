439915275eeef05410de2cf646cafe547f28bd66
Marc Weinberger <marc.weinberger@me.com>
2015-07-30 23:35:45 +0200

    added frontend generated by yeoman gulp-angular generator

---
+++ src/main/webapp/app/index.constants.js
@@ -0,0 +1,11 @@
+/* global malarkey:false, toastr:false, moment:false */
+(function () {
+  'use strict';
+
+  angular
+    .module('cvmaker')
+    .constant('malarkey', malarkey)
+    .constant('toastr', toastr)
+    .constant('moment', moment);
+
+})();
