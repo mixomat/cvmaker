7d3aadb62632bff2f1f521a52bd2e1610ab6e64a
Marc Weinberger <marc.weinberger@me.com>
2015-07-31 00:03:44 +0200

    removed unused components

--- src/main/webapp/app/main/main.controller.js
+++ src/main/webapp/app/main/main.controller.js
@@ -6,10 +6,9 @@
     .controller('MainController', MainController);
 
   /** @ngInject */
-  function MainController($timeout, webDevTec, toastr) {
+  function MainController($timeout, toastr) {
     var vm = this;
 
-    vm.awesomeThings = [];
     vm.classAnimation = '';
     vm.creationDate = 1438285936992;
     vm.showToastr = showToastr;
@@ -17,23 +16,14 @@
     activate();
 
     function activate() {
-      getWebDevTec();
       $timeout(function () {
         vm.classAnimation = 'rubberBand';
       }, 4000);
     }
 
     function showToastr() {
-      toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
+      toastr.info('Hello');
       vm.classAnimation = '';
-    }
-
-    function getWebDevTec() {
-      vm.awesomeThings = webDevTec.getTec();
-
-      angular.forEach(vm.awesomeThings, function (awesomeThing) {
-        awesomeThing.rank = Math.random();
-      });
     }
   }
 })();
