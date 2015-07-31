eb0fdd823ee5bd9adeef544bdfee201c4b9139f1
Marc Weinberger <marc.weinberger@me.com>
2015-07-31 20:52:04 +0200

    moved static app to frontend and fixed all paths

--- frontend/app/index.constants.js
+++ frontend/app/index.constants.js
@@ -1,10 +1,9 @@
-/* global malarkey:false, toastr:false, moment:false */
+/* global toastr:false, moment:false */
 (function () {
   'use strict';
 
   angular
     .module('cvmaker')
-    .constant('malarkey', malarkey)
     .constant('toastr', toastr)
     .constant('moment', moment);
 
