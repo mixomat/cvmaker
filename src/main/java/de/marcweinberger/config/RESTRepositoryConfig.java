3ee8b45a98e5cf2987ff0972bfecdceaab1127ee
Marc Weinberger <marc.weinberger@me.com>
2015-08-12 09:03:27 +0200

    exposing mongodb id

---
+++ src/main/java/de/marcweinberger/config/RESTRepositoryConfig.java
@@ -0,0 +1,21 @@
+package de.marcweinberger.config;
+
+import de.marcweinberger.model.Project;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
+
+/**
+ * TODO document me
+ *
+ * @author Marc Weinberger, marc.weinberger@me.com
+ * @since 12.08.15
+ */
+@Configuration
+public class RESTRepositoryConfig extends RepositoryRestMvcConfiguration {
+
+  @Override
+  protected void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+    config.exposeIdsFor(Project.class);
+  }
+}
