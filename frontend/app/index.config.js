1c2ba2cdf9fd1accf88139c71cf17f633536c56a
Marc Weinberger <marc.weinberger@me.com>
2015-08-16 22:55:43 +0200

    layout improvements

--- frontend/app/index.config.js
+++ frontend/app/index.config.js
@@ -17,7 +17,6 @@
     toastr.options.timeOut = 3000;
     toastr.options.positionClass = 'toast-top-right';
     toastr.options.preventDuplicates = true;
-    toastr.options.progressBar = true;
   }
 
 })();
