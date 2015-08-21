bc23ea76818601eb0e673afb059e12deecba0d80
Marc Weinberger <marc.weinberger@me.com>
2015-08-21 15:40:43 +0200

    added DELETE to cors config

--- src/main/java/de/marcweinberger/config/CORSConfig.java
+++ src/main/java/de/marcweinberger/config/CORSConfig.java
@@ -24,6 +24,7 @@ public class CorsConfig {
     config.addAllowedMethod("GET");
     config.addAllowedMethod("POST");
     config.addAllowedMethod("PUT");
+    config.addAllowedMethod("DELETE");
 
     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
     source.registerCorsConfiguration("/**", config);
