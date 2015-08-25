9f0a2e6bea0cb4decc453e826e7de861fab5d171
Marc Weinberger <marc.weinberger@me.com>
2015-08-25 17:14:11 +0200

    moved project form component to sub dir

--- frontend/app/components/project/form/project.form.directive.js
+++ frontend/app/components/project/form/project.form.directive.js
@@ -9,7 +9,7 @@
   function ProjectForm() {
     return {
       restrict: 'E',
-      templateUrl: 'app/components/project/project.form.html',
+      templateUrl: 'app/components/project/form/project.form.html',
       scope: true,
       bindToController: {
         project: '=',
