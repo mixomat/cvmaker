84ff732c19d9fd8561be16dfd0c06a404de3e8fb
Marc Weinberger <marc.weinberger@me.com>
2015-08-31 20:35:39 +0200

    created controller for technology repo

---
+++ src/main/java/de/marcweinberger/controller/TechnologyController.java
@@ -0,0 +1,31 @@
+package de.marcweinberger.controller;
+
+import de.marcweinberger.model.Technology;
+import de.marcweinberger.repository.TechnologyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.BasePathAwareController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+/**
+ * Simple controller for Technology resource.
+ *
+ * @author Marc Weinberger, marc.weinberger@me.com
+ * @since 31.08.15
+ */
+@BasePathAwareController
+public class TechnologyController {
+
+  @Autowired
+  private TechnologyRepository technologyRepository;
+
+
+  @RequestMapping(method = RequestMethod.GET, value = "technologies")
+  @ResponseBody
+  public List<Technology> all() {
+    return technologyRepository.all();
+  }
+}
